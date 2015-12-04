4fe4412def9ae79c6b87d59b57e76c60f29b3754
Mattie <mattie.carlson32@gmail.com>
2015-12-03 21:57:46 -0800

    Added teamNum parameter to each method

--- private_modules/voteCounter.js
+++ private_modules/voteCounter.js
@@ -1,23 +1,47 @@
 module.exports = {
 
     NUM_SPOTS: 100,
-    spots: [],
-    max: 0,
-    maxIndex: -1,
+    teamOneSpots: [],
+    teamTwoSpots: [],
 
-    clearVotes: function() {
-        this.spots = [];
+    // maxArray[0] = team one max
+    // maxArray[1] = team one max index
+    // maxArray[2] = team two max
+    // maxArray[3] = team two max index
+    maxArray: [0, -1, 0, -1],
+
+    clearVotes: function(teamNum) {
+        var spots = this.pickTeamArray(teamNum);
+        this.maxArray[this.calculateIndexOfMax(teamNum)] = 0;
+        this.maxArray[this.calculateIndexOfMaxIndex(teamNum)] = -1;
+        spots = [];
     },
 
-    vote: function(location) {
-        this.spots[location] = this.spots[location]++;
-        if (this.spots[location] > this.max) {
-            this.max = this.spots[location];
-            this.maxIndex = location;
+    vote: function(teamNum, location) {
+        var spots = this.pickTeamArray(teamNum);
+        spots[location] = spots[location]++;
+        if (spots[location] > this.maxArray[this.calculateIndexOfMax(teamNum)]) {
+            this.maxArray[this.calculateIndexOfMax(teamNum)] = spots[location];
+            this.maxArray[this.calculateIndexOfMaxIndex(teamNum)] = location;
         }
     },
 
-    getSpotWithMostVotes: function() {
-        return maxIndex;
+    // Returns -1 if no votes received since last clear, 
+    // else index of spot with most votes
+    getSpotWithMostVotes: function(teamNum) {
+        return this.maxArray[this.calculateIndexOfMaxIndex(teamNum)];
+    },
+
+    pickTeamArray: function(teamNum) {
+        return (teamNum == 1) ? teamOneSpots : teamTwoSpots;
+    },
+
+    calculateIndexOfMax: function(teamNum) {
+        return 2 * (teamNum - 1);
+    },
+
+    calculateIndexOfMaxIndex: function(teamNum) {
+        return 2 * (teamNum - 1) + 1;
     }
+
 }
