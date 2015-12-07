df3548c7055a3d8ce744dee5572c2acde18c0e58
kritinv <kritinvij@gmail.com>
2015-12-07 05:22:12 -0800

    Front end baby

--- public/gameboard.js
+++ public/gameboard.js
@@ -26,10 +26,12 @@
         });
 
 
+        var elem;
         //Click handler for cells
         for(var i = 1; i <= ROWS*COLS; i++) {
             $("#" + i + "enemyBoard").click(function() {
                 connection.postVote(this.id);
+                $("#vote").html(parseInt(this.id));
             });
         }
  
@@ -41,10 +43,18 @@
     });
 
     // hit = 8, 6, 4, 2, 0
-        // miss = 12
-        // 
+    // miss = 12
     var updateBoardStateFunction = function(boardState) {
         var boards = JSON.parse(boardState);
+        var boards2 = boards.slice();
+        boards2 = boards2.splice(100, 100);
+
+        var sunk2 = containsElementNtimes(boards2, 8, 2);
+        var sunk3a = containsElementNtimes(boards2, 6, 3);
+        var sunk3b = containsElementNtimes(boards2, 4, 3);
+        var sunk4 = containsElementNtimes(boards2, 2, 4);
+        var sunk5 = containsElementNtimes(boards2, 0, 5);
+
         for(var i = 1; i <= ROWS*COLS; i++) {
             var state = boards[i+99];
             //$("#" + i + "enemyBoard").html(state);
@@ -54,16 +64,42 @@
                 td.className = "miss";
             } else if (state % 2 == 0) {
                 td.className = "hit";
+                if (sunk2 && state == 8) {
+                    td.className = "pointer2";
+                }
+
+                if (sunk3a && state == 6) {
+                    td.className = "pointer3a";
+                }
+
+                if (sunk3b && state == 4) {
+                    td.className = "pointer3b";
+                }
+
+                if (sunk4 && state == 2) {
+                    td.className = "pointer4";
+                }
+
+                if (sunk5 && state == 0) {
+                    td.className = "pointer5";
+                }
             }
         }
 
-        for(var i = 1; i <= ROWS*COLS; i++) {
+        for (var i = 1; i <= ROWS*COLS; i++) {
             var state = boards[i-1];
             $("#" + i + "teamBoard").html(state);
             var td = $("#" + i + "teamBoard")[0];
-            if(state % 2 == 1 && state != 13) {
-                //ship
-                td.className = "ship";
+            if (state == 9) {
+                td.className = "pointer2";
+            } else if (state == 7){
+                td.className = "pointer3a";
+            } else if (state == 5){
+                td.className = "pointer3b";
+            } else if (state == 3){
+                td.className = "pointer4";
+            } else if (state == 1){
+                td.className = "pointer5";
             } else {
                 td.className = "sea";
             }
@@ -74,6 +110,17 @@
             }
         }
     };
+
+    function containsElementNtimes(boards, x, n) {
+        var count = 0;
+        for (var i = 0; i < ROWS*COLS; i++) {
+            if (boards[i] == x) {
+                count++;
+            }
+        }
+
+        return (count == n);
+    }
 
     function buildGameBoard(rows, cols, divName) {
         var tableDiv = $("#" + divName);
@@ -106,7 +153,7 @@
 
     setInterval(function(){ 
         timeLeft--;
-        $("#timer").html(timeLeft); 
+        $("#timer").html(timeLeft);
         if(timeLeft == 0) {
             timeLeft = turnLength;
             whosTurn = whosTurn % 2 + 1;
